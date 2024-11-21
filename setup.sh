6ee40a03f16a6fad6ff19882930ae16ca4170ed1
lakshmikanth <lkapp544@gmail.com>
2024-11-21 12:50:40 +0000

    fix 1

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 curl -fsSL https://ollama.com/install.sh | sh
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh 
+
+bash
 
 nvm install 22
 
