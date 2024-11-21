c6e56ace07d22e2f0ab540f57fe7997f0d9ab87e
Lakshmikanth <k.lakshmikanth002@gmail.com>
2024-11-21 17:03:01 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+curl -fsSL https://ollama.com/install.sh | sh
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
+
+nvm install 22
+
+npm install -g flowise@next
+
+npm install -g n8n@next
