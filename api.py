dc850726eaa52362008729b8649a1622c2980fcc
lakshmikanth <lkapp544@gmail.com>
2024-11-27 16:39:55 +0000

    modified api.py

--- api.py
+++ api.py
@@ -5,12 +5,21 @@ import os
 
 app = Flask(__name__)
 
+@app.route("/api/chat", methods=["GET"])
+def chat():
+    """User prompt formating"""
+    return {"user_prompt":request.args["body"]} , 200
+
 @app.route("/api/transcribe", methods=["GET"])
 def transcribe():
     """Retrieve text from audio."""
 
-    filename = request.args["path"]
-    
+    filename = f"/workspaces/lk-server/n8n/data/{request.args['file']}"
+    audio_path = request.args["path"]
+
+    with open(filename, "wb") as f:
+        f.write(req.get(audio_path).content)
+
     transcription = req.post("https://api.groq.com/openai/v1/audio/transcriptions",
                     headers={"Authorization": "Bearer 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"},
                     data={"model": "whisper-large-v3-turbo",
@@ -19,7 +28,7 @@ def transcribe():
     
     os.remove(filename)
 
-    return transcription.json() , 200
+    return {"user_prompt":transcription.json()["text"]} , 200
 
 if __name__ == "__main__":
     app.run(debug=True,port=3000)
