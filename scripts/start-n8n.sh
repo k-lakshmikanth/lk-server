925532d45c53d811815f546b87f2e7ab23ffc984
lakshmikanth <lkapp544@gmail.com>
2024-12-17 09:48:29 +0000

    updated api and changed webhook_url in script

--- scripts/start-n8n.sh
+++ scripts/start-n8n.sh
@@ -1,2 +1,2 @@
-export WEBHOOK_URL=https://silver-barnacle-w64jx7qwj736r9-5678.app.github.dev/
+export WEBHOOK_URL=https://probable-computing-machine-4pxgx9v7w96hqjww-5678.app.github.dev/
 n8n start
